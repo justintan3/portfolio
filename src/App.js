4b0683d722cecd6224334bfe4d4c6e96581c28e4
Justin Tan <justintan@Justins-MacBook-Air.local>
2024-01-06 09:23:27 -0800

    change my-name and occupation placement

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import Home from './pages/Home';
-import About from './pages/About';
-import Experiences from './pages/Experiences';
-import Contact from './pages/Contact';
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Experiences from "./pages/Experiences";
+import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 const App = () => {
-
   return (
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
 
-        <>
-          <Router>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />}/>
-              <Route path='/experiences' element={<Experiences />}/>
-              <Route path='/Contact' element={<Contact />}/>
-            </Routes>
-          </Router>
-        </>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/experiences" element={<Experiences />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
     //     <div className="introduction">
     //       <h1 className="hello">
     //         Hello, I'm
@@ -30,11 +29,11 @@ const App = () => {
     //         Justin Tan
     //       </h2>
     //       </h1>
-          
+
     //     </div>
     //   </header>
     // </div>
   );
-}
+};
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,57 +1,73 @@
-import React, {useRef} from 'react';
-import Navbar from '../components/Navbar';
-import About from './About';
-import Experiences from './Experiences';
-import Contact from './Contact';
-import Background from '../background_img.png';
+import React, { useRef } from "react";
+import Navbar from "../components/Navbar";
+import About from "./About";
+import Experiences from "./Experiences";
+import Contact from "./Contact";
+import Background from "../background_img.png";
+import "./pages.css";
 
 export const Home = () => {
-    const homeRef = useRef(null);
-    const aboutRef = useRef(null);
-    const experiencesRef = useRef(null);
-    const contactRef = useRef(null);
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const experiencesRef = useRef(null);
+  const contactRef = useRef(null);
 
-    const handleScroll = (ref) => {
-        window.scrollTo({
-          top: ref.offsetTop,
-          left: 0,
-          behavior: "smooth",
-        });
-      };
+  const handleScroll = (ref) => {
+    window.scrollTo({
+      top: ref.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
 
-      console.log("homeRef: ", homeRef);
-      console.log("aboutRef: ", aboutRef)
+  console.log("homeRef: ", homeRef);
+  console.log("aboutRef: ", aboutRef);
   return (
     <>
-    <div className="App" ref={homeRef}>
-      <header className="App-header">
-        <img src={Background} className="App-logo" alt="logo" />
-        <div className="nav-panel">
-        <Navbar homeRef={homeRef} aboutRef={aboutRef} experiencesRef={experiencesRef} contactRef={contactRef} handleScroll={handleScroll}/>
-    </div>
-    <div className="introduction">
-          <h1 className="hello">
-            Hello, I'm
-            <p className="my-name">
-            Justin Tan
-          </p>
-          <p className="hello">Frontend Developer</p>
-          </h1>
-          
-        </div>
-      </header>
-    </div>
-    <div ref={aboutRef}>
+      <div className="App" ref={homeRef}>
+        <header className="App-header">
+          <img src={Background} className="App-logo" alt="logo" />
+          <div className="nav-panel">
+            <Navbar
+              homeRef={homeRef}
+              aboutRef={aboutRef}
+              experiencesRef={experiencesRef}
+              contactRef={contactRef}
+              handleScroll={handleScroll}
+            />
+          </div>
+          <div className="introduction">
+            <div className="top-bar">
+              {/* <h1 className="hello">
+                Hello, I'm */}
+              <p className="my-name" rel="stylesheet">
+                Justin Tan
+              </p>
+              <p className="occupation">Frontend Developer</p>
+              {/* </h1> */}
+            </div>
+            <div className="i-am-him">
+              <p>
+                I am a Frontend Developer based in Los Angeles. I have multiple
+                years of experience in developing a quality web-based
+                applications. I enjoy exploring aesthetic and minimalistic
+                designs. I love traveling, coffee, and food.
+              </p>
+            </div>
+          </div>
+        </header>
+      </div>
+      <div ref={aboutRef}>
         <About />
-    </div>
-    <div ref={experiencesRef}>
+      </div>
+      <div ref={experiencesRef}>
         <Experiences />
-    </div>
-    <div ref={contactRef}>
+      </div>
+      <div ref={contactRef}>
         <Contact />
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
