aeafc67fac2f73026edbc402d7897c0de7b0be84
justintan3 <justintan397@gmail.com>
2024-01-08 22:45:42 -0800

    fix experience page spacing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,91 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import MenuIcon from '../menu-icon.svg';
-import FBicon from '../fb-icon.svg';
-import INicon from '../in-icon.svg';
-import GHicon from '../gh-icon.svg';
-import './Navbar.css'
+import MenuIcon from "../menu-icon.svg";
+import FBicon from "../fb-icon.svg";
+import INicon from "../in-icon.svg";
+import GHicon from "../gh-icon.svg";
+import "./Navbar.css";
 
-const Navbar = ({ handleScroll, homeRef, aboutRef, experiencesRef, contactRef }) => {
+const Navbar = ({
+  handleScroll,
+  homeRef,
+  aboutRef,
+  experiencesRef,
+  contactRef,
+}) => {
   return (
-      <div className="nav">
-        <ul className="nav-menu">
-          <li>
-            <Link to="/" onClick={() => {handleScroll(homeRef.current);}}>HOME</Link>
-          </li>
-          <li><Link to="/" onClick={() => {handleScroll(aboutRef.current);}}>ABOUT</Link></li>
-          <li><Link to="/" onClick={() => {handleScroll(experiencesRef.current);}}>PROJECT</Link></li>
-          <li><Link to="/" onClick={() => {handleScroll(contactRef.current);}}>CONTACT</Link></li>
-        </ul>
-        <ul className="media-menu">
-          <li>
-            <Link to={{ pathname: "https://www.facebook.com/justin.tan.547/"}}>
-              <img src={FBicon} className="media-links" alt='fb-icon' />
-            </Link>
-          </li>
-          <li>
-            <Link to={{ pathname: "https://www.linkedin.com/in/justin-397-tan/"}}>
-              <img src={INicon} className="media-links" alt='fb-icon' />
-            </Link>
-          </li>
-          <li>
-            <Link to={{ pathname: "https://github.com/justintan3"}}>
-              <img src={GHicon} className="media-links" alt='fb-icon' />
-            </Link>
-          </li>
-        </ul>
-        
-      </div>
-      // <> 
-      //   <Routes>
-      //     {/* <Route path="/" element={<Home />} /> */}
-      //     <Route path="/about" element={<About />} />
-      //     <Route path="/portfolio" element={<Portfolio />} />
-      //     <Route path="/contact" element={<Contact />} />
-      //   </Routes>
-      // </>
-  )
-}
+    <div className="nav">
+      <ul className="nav-menu">
+        <li>
+          <Link
+            to="/"
+            onClick={() => {
+              handleScroll(homeRef.current);
+            }}
+          >
+            HOME
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/"
+            onClick={() => {
+              handleScroll(aboutRef.current);
+            }}
+          >
+            ABOUT
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/"
+            onClick={() => {
+              handleScroll(experiencesRef.current);
+            }}
+          >
+            EXPERIENCES
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/"
+            onClick={() => {
+              handleScroll(contactRef.current);
+            }}
+          >
+            CONTACT
+          </Link>
+        </li>
+      </ul>
+      <ul className="media-menu">
+        <li>
+          <Link to={{ pathname: "https://www.facebook.com/justin.tan.547/" }}>
+            <img src={FBicon} className="media-links" alt="fb-icon" />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to={{ pathname: "https://www.linkedin.com/in/justin-397-tan/" }}
+            target="_blank"
+          >
+            <img src={INicon} className="media-links" alt="fb-icon" />
+          </Link>
+        </li>
+        <li>
+          <Link to={{ pathname: "https://github.com/justintan3" }}>
+            <img src={GHicon} className="media-links" alt="fb-icon" />
+          </Link>
+        </li>
+      </ul>
+    </div>
+    // <>
+    //   <Routes>
+    //     {/* <Route path="/" element={<Home />} /> */}
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/portfolio" element={<Portfolio />} />
+    //     <Route path="/contact" element={<Contact />} />
+    //   </Routes>
+    // </>
+  );
+};
 export default Navbar;
--- src/pages/Experiences.js
+++ src/pages/Experiences.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import './pages.css';
+import React from "react";
+import Navbar from "../components/Navbar";
+import "./pages.css";
 
 export const Project = () => {
-    return (
-        <div className="App">
-            <div className="nav-panel">
-                <Navbar />
-            </div> 
-            <div className='project-container'>
-                <div className="header">Who am I.</div>
-                {/* <div className="experience">
-                    Led frontend development of multiple successfully launched internal web-based applications
-                    that is now used by hundreds of employees company-wide.
-                </div>
-                <div className="experience">
-                    Worked with other developers, DevOps, QA, and business users in an agile environment to effectively deliver features and resolve issues in a timely manner.
-                </div> */}
-                <div className="experience-container">
-                <div className="divider" style={{ height: "215px", top: "0%" }}></div>
-                </div>
-                <div className="divider" style={{ height: "215px", top: "500px", left: "-10%"}}></div>
-            </div>
-
+  return (
+    <div className="App">
+      <div className="nav-panel">
+        <Navbar />
+      </div>
+      <div className="project-container">
+        <div className="header">Experiences</div>
+        {/* <div className="experience">
+          Led frontend development of multiple successfully launched internal
+          web-based applications that is now used by hundreds of employees
+          company-wide.
         </div>
-    )
-}
+        <div className="experience">
+          Worked with other developers, DevOps, QA, and business users in an
+          agile environment to effectively deliver features and resolve issues
+          in a timely manner.
+        </div> */}
+        <div className="experience-container-1">
+          <div
+            className="experience-divider"
+            //   style={{ height: "215px", top: "0%" }}
+          ></div>
+        </div>
+        <div className="experience-container-2">
+          <div
+            className="experience-divider"
+            // style={{ height: "215px", top: "500px", left: "-10%" }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Project;
