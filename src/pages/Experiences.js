f8be649d83549d065ec0dfce140ee765aec3bbf5
justintan3 <justintan397@gmail.com>
2024-01-16 08:57:13 -0800

    align experience page and add projects

--- src/pages/Experiences.js
+++ src/pages/Experiences.js
@@ -8,7 +8,7 @@ export const Project = () => {
       <div className="nav-panel">
         <Navbar />
       </div>
-      <div className="project-container">
+      <div className="experience-page-container">
         <div className="header">Experiences</div>
         {/* <div className="experience">
           Led frontend development of multiple successfully launched internal
@@ -20,24 +20,39 @@ export const Project = () => {
           agile environment to effectively deliver features and resolve issues
           in a timely manner.
         </div> */}
-        <div className="experience-container-1">
+        <div className="experience-container">
           <div
             className="experience-divider"
             //   style={{ height: "215px", top: "0%" }}
           ></div>
-          <p className="project-name">
-            Document Approval
-            <ul>
-              <li className="project-description">
-                Led frontend development of multiple successfully launched
-                internal web-based applications that is now used by hundreds of
-                employees company-wide.
-              </li>
-            </ul>
-          </p>
+          <p className="experience-name">Document Approval</p>
+          <ul>
+            <li className="experience-description">
+              Led frontend development of multiple successfully launched
+              internal web-based applications that is now used by hundreds of
+              employees company-wide. Collaborated with cross-functional teams
+              in an agile environment to deliver features and resolve issues
+              efficiently. Designed and implemented unit tests, integration
+              tests, and automated tests using Cypress for consistent code
+              quality. Optimized multiple critical applications for improving
+              file upload speed by 5x and improving scalability.
+            </li>
+          </ul>
         </div>
-        <div className="experience-container-2">
-          <div className="experience-divider" style={{ left: "0%" }}></div>
+        <div className="project-header">Projects</div>
+        <div className="project-container">
+          <div className="experience-divider" style={{ left: "0%" }} />
+          <p className="project-name">Document Approval</p>
+          <ul>
+            <li className="project-description">
+              Developed a document approval application built with React, SCSS,
+              Hasura, and Azure.
+            </li>
+            <li className="project-description">
+              Implemented Cypress automated testing to conduct code reviews to
+              instantly detect bugs and issues.
+            </li>
+          </ul>
         </div>
       </div>
     </div>
